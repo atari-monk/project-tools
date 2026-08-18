6b109f06640b7b335acd1786f55ea15d4b9c7a95
atari-monk <atari.monk1@gmail.com>
2026-08-18 17:11:11 +0200

    chore: rename src files

--- src/project_tools/cli.py
+++ src/project_tools/cli.py
@@ -1,7 +1,7 @@
 from project_tools.const import WORKSPACE_PATH
 from project_tools.file_system import create_folder
 from project_tools.logger import setup_logger
-from project_tools.setup_argparse import setup_argparse
+from project_tools.argparse_setup import setup_argparse
 
 
 def main() -> None:
