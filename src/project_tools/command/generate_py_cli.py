33eb19b8b8f9a8e7ef28b480c0fcfd7366c3ce7c
atari-monk <atari.monk1@gmail.com>
2026-09-04 14:00:35 +0200

    fix: model bug in py cli generator

--- src/project_tools/command/generate_py_cli.py
+++ src/project_tools/command/generate_py_cli.py
@@ -4,13 +4,11 @@ import logging
 from project_tools.config import WORKSPACE_PATH
 from project_tools.modules.project_py_cli.data_model import set_py_cli_project
 from project_tools.modules.project_py_cli.generator import create_py_cli_project
-from project_tools.modules.project_shared.data_model import set_project
 
 
 logger = logging.getLogger(__name__)
 
 
 def run(args: Namespace) -> None:
-    proj_config = set_project(args.project, args.description, WORKSPACE_PATH)
-    pycli_config = set_py_cli_project(proj_config, args.cli_name)
+    pycli_config = set_py_cli_project(args.project, args.description, WORKSPACE_PATH, args.cli_name)
     create_py_cli_project(pycli_config, logger)
